00fa2fdac124f4e1cd3db64c10187601dac36782
Davi Costa <davi.machado.costa@hotmail.com>
2020-10-06 15:14:09 -0300

    First commit

---
+++ Controllers/EmptyController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMVC.Controllers
+{
+    public class EmptyController : Controller
+    {
+        public string Index()
+        {
+            return "Sou um Controller vazio ;-;";
+        }
+
+        public string Saudacao(string nome)
+        {
+            return $"Ola {nome}, seja bem vindo!";
+        }
+    }
+}
---
+++ Data/AspNetMVCContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AspNetMVC.Models;
+
+namespace AspNetMVC.Data
+{
+    public class AspNetMVCContext : DbContext
+    {
+        public AspNetMVCContext (DbContextOptions<AspNetMVCContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+
+        public DbSet<AspNetMVC.Models.Contact> Contact { get; set; }
+    }
+}
